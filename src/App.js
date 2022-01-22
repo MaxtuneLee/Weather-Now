b85b38e5bb20b5ce6352e262b8d8d7844741c343
MaxtuneLee <60775796+MaxtuneLee@users.noreply.github.com>
2022-01-22 23:52:04 +0800

    maincard目前已经正常显示温度和提示，目前正在弄图标

--- src/App.js
+++ src/App.js
@@ -3,62 +3,91 @@ import {weatherId,weatherIp} from './weatherApi';
 import React,{ Fragment, useState } from 'react';
 import {Layout, Row, Col, Divider, Cascader, Card, Typography, AutoComplete,Progress} from 'antd';
 import options from  './cities.js'
-import Icon,{EnvironmentFilled,FieldTimeOutlined,CalendarOutlined} from '@ant-design/icons'
+import Icon,{EnvironmentFilled,FieldTimeOutlined,CalendarOutlined,createFromIconfontCN} from '@ant-design/icons'
 import {Sunny} from './icons'
 import axios from 'axios';
 import { param } from 'jquery';
 import './axiosjsonp'
 import reactDom from 'react-dom';
+import './iconfont'
 
 
 const {Title} = Typography;
 const {Meta} = Card;
 const SunnyIcon = props => <Icon component={Sunny} {...props} />;
-const {Header, Footer, Sider, Content} = Layout
-let city='正在定位';
-let tem,temDay,temNight;
+const {Header, Footer, Content} = Layout
+let city,tem,temDay,temNight,weatherDayImg,weatherDay;
+const IconFont = createFromIconfontCN({
+  scriptUrl:'//at.alicdn.com/t/font_3157781_ce79q9xjds9.js'
+})
 
-axios.get('https://v0.yiketianqi.com/api',{
-  params:{
-    appid:'52311237',
-    appsecret:'o4CyyoqO',
-    version:'v9',
-    city:'广州',
+class App extends React.Component {
+  state={
+    city:'',
+    data:[],
+    aqi:{}
   }
-}).then(
-  function(response){
-    // console.log(response);
-    city=response.data.city;
-    
+  updateData=(city)=>{
+    console.log(city)
+    this.setState({city})
   }
-)
-
-function App() {
-  return (
-    <>
+  componentWillMount(){
+    axios.get('https://v0.yiketianqi.com/api',{
+      params:{
+        appid:'23035354',
+        appsecret:'8YvlPNrz',
+        version:'v9',
+        city:'海南',
+      }
+    }).then(
+      (response)=>{
+        console.log(response.data);
+        this.setState(response.data)
+        console.log(this.state)
+      }
+    )
+    .catch(function (error) {
+      console.log(error)
+    })
+  }
+  render (){
+    return(
       <Layout>
-        <Header className='Header'>
+        <HeaderWN {...this.state}/>
+        <ContentWN {...this.state} updateData={this.props.updateData}/>
+        <Footer>&copy;2022 Maxtune</Footer>
+      </Layout>
+      )
+  }
+}
+
+class HeaderWN extends React.Component {
+  render(){
+    let {city,data,aqi} = this.props
+    console.log('header city is:',city)
+    return (
+      <Header className='Header'>
           <Row justify="space-between" align="middle">
             <span className='title'>Weather Now</span>
             <div>
               <EnvironmentFilled style={{color:'rgb(39, 39, 39)'}}/>
               &nbsp;
-              <CitySwitcher />
+              <CitySwitcher localcity={city}/>
             </div>
           </Row>
         </Header>
-        <Content className='content'>
+    )
+  }
+}
+
+class ContentWN extends React.Component {
+  render(){
+    let {city,data,aqi} = this.props
+    console.log('contentWN city is:',city)
+    return(
+      <Content className='content'>
           <div className='Box' style={{marginLeft:'auto',marginRight:'auto'}}>
-            <Row justify='space-between' align='middle' className='mainCard'>
-              <div className='greet'> : ) 天气很不错，不如出去走一走吧！</div>
-              <div className='weatherCard'>
-                <span className='tempreture'>20°C</span>
-                <div className='dayAndNight'><span>日间 25°C</span><span>夜间 18°C</span></div>
-                <div className='tempNow'>
-                  <SunnyIcon/>
-                </div>
-              </div>
-            </Row>
+            <MainCard {...this.props}/>
             <div className='hourData'>
               <Title level={4}><FieldTimeOutlined /> 逐小时天气</Title>
               <div className='hours'>
@@ -307,24 +336,26 @@ function App() {
             </div>
           </div>
         </Content>
-        <Footer>&copy;2022 Maxtune</Footer>
-      </Layout>
-    </>
-  );
+    )
+  }
 }
 
 class CitySwitcher extends React.Component {
   state = {
-    text: city,
+    text: '正在加载',
   };
   onChange = (value, selectedOptions) => {
     this.setState({
       text: selectedOptions.map(o => o.label).join(', '),
     });
-    city = selectedOptions[selectedOptions.length-1].code;
-    console.log(selectedOptions[selectedOptions.length-1].code);
+    city = selectedOptions[selectedOptions.length-2].code;
+    console.log(selectedOptions[selectedOptions.length-2].code);
   };
-
+  componentWillMount(){
+    setTimeout(() => {
+      this.setState({text:this.props.localcity})
+    }, 3000);
+  }
   render() {
     return (
       <span>
@@ -338,4 +369,58 @@ class CitySwitcher extends React.Component {
   }
 }
 
-export default App; export {city};
+class MainCard extends React.Component {
+  state = {
+    greet: '?_? 天气数据正在加载',
+    tem:'--',
+    temDay:'--',
+    temNight:'--',
+    weatherDayImg:weatherDayImg,
+  };
+  greet(wea){
+    if(wea==='晴'){
+      return ': ) 天气不错，出去走走吧'
+    }
+    if(wea==='阴'||wea==='多云'){
+      return ': | 今天是天气一般呢，但也可以出去走走'
+    }
+    if(wea==='小雨'||wea==='暴雨'){
+      return ': ( 今天居然下雨了。。睡个懒觉吧'
+    }
+    if(wea==='小雪'){
+      return ': ) 下雪咯，好欸！！'
+    }
+    else{
+      return ': ) 又是新的一天呢，也要干劲满满哦'
+    }
+  };
+
+  componentWillMount(){
+    setTimeout(() => {
+      let localData = this.props
+      console.log('maincard datatem is:',localData.data[0].tem)
+      this.setState({
+        greet:this.greet(localData.data[0].wea_day),
+        tem:localData.data[0].tem,
+        temDay:localData.data[0].tem1,
+        temNight:localData.data[0].tem2,
+      })
+    }, 3000);
+  }
+  render() {
+    return (
+      <Row justify='space-between' align='middle' className='mainCard'>
+              <div className='greet'> {this.state.greet}</div>
+              <div className='weatherCard'>
+                <span className='tempreture'>{this.state.tem}°C</span>
+                <div className='dayAndNight'><span>日间 {this.state.temDay}°C</span><span>夜间 {this.state.temNight}°C</span></div>
+                <div className='tempNow'>
+                  <SunnyIcon/>
+                </div>
+              </div>
+            </Row>
+    );
+  }
+}
+
+export default App;
